6c518830fd8a4e0bdc56629b365a69ba73cc26ec
Andrew Teich <andrewteich@me.com>
2014-11-10 14:45:04 -0800

    Added images linked to project

---
+++ CrowdCrunch/PathTransformer.h
@@ -0,0 +1,17 @@
+//
+//  PathTransformer.h
+//  CrowdCrunch
+//
+//  Created by HackReactor on 11/10/14.
+//  Copyright (c) 2014 HackReactor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PathTransformer : NSValueTransformer {
+    
+}
+
+- (id) transformedValue:(id)value;
+
+@end
--- CrowdCrunch/ViewController.m
+++ CrowdCrunch/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "Project.h"
+#import "PathTransformer.h"
 
 @implementation ViewController
 @synthesize projects;
@@ -19,23 +20,27 @@
     NSCollectionViewItem *proto = [self.storyboard instantiateControllerWithIdentifier:@"projectItemView"];
     [collectionView setItemPrototype:proto];
     
+    id transformer = [[PathTransformer alloc] init];
+    [NSValueTransformer setValueTransformer:transformer forName:@"PathTransformer"];
+
+    
     // Do any additional setup after loading the view.
     Project * pm1 = [[Project alloc] init];
     pm1.title = @"John Appleseed";
     pm1.text = @"Doctor";
-    pm1.image = @"Image1";
+    pm1.image = @"fibre-optic.jpg";
     pm1.subscribed = @"NO";
     
     Project * pm2 = [[Project alloc] init];
     pm2.title = @"Jane Carson";
     pm2.text = @"Teacher";
-    pm2.image = @"Image2";
+    pm2.image = @"fibre-optic.jpg";
     pm2.subscribed = @"YES";
     
     Project * pm3 = [[Project alloc] init];
     pm3.title = @"Ben Alexander";
     pm3.text = @"Student";
-    pm3.image = @"Image2";
+    pm3.image = @"fibre-optic.jpg";
     pm3.subscribed = @"NO";
     
     NSMutableArray * tempArray = [NSMutableArray arrayWithObjects:pm1, pm2, pm3, nil];
