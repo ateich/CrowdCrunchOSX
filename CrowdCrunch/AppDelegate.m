3a6b26f70ed000ad641b49a1da1c3568ab7cb20a
Andrew Teich <andrewteich@me.com>
2014-11-17 12:06:34 -0800

    Listens for changes to power source

--- CrowdCrunch/AppDelegate.m
+++ CrowdCrunch/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <IOKit/ps/IOPowerSources.h>
+
 
 @interface AppDelegate (){
     NSString *path;
@@ -26,11 +28,17 @@
     [task setLaunchPath:@"/bin/bash"];
     [task setArguments:[NSArray arrayWithObjects:[[NSBundle mainBundle] pathForResource:@"startDocker" ofType:@"sh"], nil]];
     [task launch];
+    [self initializePowerSourceChanges];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
     [data writeToFile: path atomically:YES];
+}
+
+-(int)getBatteryPercentage{
+    NSLog(@ "POWER SOURCES: %@", IOPSCopyPowerSourcesInfo());
+    return 0;
 }
 
 //Set Setting
@@ -50,13 +58,43 @@
     NSString *documentsDirectory = [paths objectAtIndex:0];
     path = [documentsDirectory stringByAppendingPathComponent:@"settings.plist"];
     NSFileManager *fileManager = [NSFileManager defaultManager];
-
+    
     if (![fileManager fileExistsAtPath: path]){
         NSString *bundle = [[NSBundle mainBundle] pathForResource:@"settings" ofType:@"plist"];
         [fileManager copyItemAtPath:bundle toPath: path error:&error];
     }
     
     data = [[NSMutableDictionary alloc] initWithContentsOfFile: path];
+}
+
+//Battery Status
+void PowerSourcesHaveChanged(void *context){
+    NSLog(@"battery status changed");
+    
+    //    [self doThisPlease]; // <- self undeclared
+    //update battery percentage and status (plugged in or not)
+    id callbackObject = (__bridge id) context;
+    [callbackObject getBatteryPercentage];
+}
+
+/* initializePowerSourceChanges
+ *
+ * Registers a handler that gets called on power source (battery or UPS)
+ changes
+ */
+-(void)initializePowerSourceChanges
+{
+    CFRunLoopSourceRef         CFrls;
+    
+    // Create and add RunLoopSource
+    CFrls =
+    IOPSNotificationCreateRunLoopSource(PowerSourcesHaveChanged, (__bridge void *)self);
+    if(CFrls) {
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), CFrls,
+                           kCFRunLoopDefaultMode);
+        CFRelease(CFrls);
+    }
+    
 }
 
 @end
